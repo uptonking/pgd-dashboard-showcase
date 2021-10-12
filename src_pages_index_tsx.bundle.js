0be9ad4a6c9f9838d0d0779495013cddf06ed43d
uptonking <jinyaoo86@gmail.com>
2021-10-12 21:58:52 +0800

    chore: initial commit as dashboard demo

---
+++ src_pages_index_tsx.bundle.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_datalking_nostalgia_dashboard"] = self["webpackChunk_datalking_nostalgia_dashboard"] || []).push([["src_pages_index_tsx"],{
+
+/***/ "./src/pages/index.tsx":
+/*!*****************************!*\
+  !*** ./src/pages/index.tsx ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _views_dashboard_starter_DashboardStarter__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../views/dashboard-starter/DashboardStarter */ \"./src/views/dashboard-starter/DashboardStarter.tsx\");\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_views_dashboard_starter_DashboardStarter__WEBPACK_IMPORTED_MODULE_0__[\"default\"]);//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvcGFnZXMvaW5kZXgudHN4LmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFFQSxpRUFBZUEsaUZBQWYiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9AZGF0YWxraW5nL25vc3RhbGdpYS1kYXNoYm9hcmQvLi9zcmMvcGFnZXMvaW5kZXgudHN4PzQxZTAiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IERhc2hib2FyZFN0YXJ0ZXIgZnJvbSAnLi4vdmlld3MvZGFzaGJvYXJkLXN0YXJ0ZXIvRGFzaGJvYXJkU3RhcnRlcic7XG5cbmV4cG9ydCBkZWZhdWx0IERhc2hib2FyZFN0YXJ0ZXI7XG4iXSwibmFtZXMiOlsiRGFzaGJvYXJkU3RhcnRlciJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/pages/index.tsx\n");
+
+/***/ })
+
+}]);
